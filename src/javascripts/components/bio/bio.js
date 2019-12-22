0ea52a6dc963ca0ec982436fa7ccc67d58b533ee
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-12-22 17:27:32 -0600

    added footer

--- src/javascripts/components/bio/bio.js
+++ src/javascripts/components/bio/bio.js
@@ -6,8 +6,8 @@ const bioString = () => {
   domString += '<img  id="me" src="https://reactnativestarter-hosting-mobilehub-132039435.s3.amazonaws.com/headshot.jpg" alt=""/>';
   domString += '<h2>About Me</h2>';
   domString += '<p> I was born and rasied in Nashville, TN. Prior to NSS, I received both my BBA and MBA with an emphasis in Finance from MTSU.';
-  domString += '<p>I currently work for the State of Tennessee in the Department of General Services as an Real Property Agent. My area of expertise is to support the various state agencies in their real estate transactions. I have an combined 20 years of experience in retail banking, financial services, and real estate.    </p>';
-  domString += '<p> My way for becoming a software developer is to teach and expose at-risk male youth to coding in addition to having the ability to provide my family a comfortable lifestyle.</p>';
+  domString += '<p>I currently work for the State of Tennessee in the Department of General Services as an Real Property Agent. My area of expertise is to support the various state agencies in their real estate transactions. I have over 20 years of business experience in retail banking, financial markets, and real estate.    </p>';
+  domString += '<p> My why for becoming a software developer is to teach and expose at-risk male youth to coding in addition to having the ability to provide my family a comfortable lifestyle.</p>';
   domString += '<p>This site displays the individual and group projects I completed while in the front-end bootcamp at NSS as well as the technologies incorporated in these projects.</p>';
   utils.printToDom('bioPage', domString);
 };
