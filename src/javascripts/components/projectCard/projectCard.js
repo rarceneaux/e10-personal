dbaa9966c293a9df41d7f339a18842daf10403c7
Raymond Arceneaux <raymond.techcoder@gmail.com>
2019-12-29 22:06:04 -0600

    update email and projects

--- src/javascripts/components/projectCard/projectCard.js
+++ src/javascripts/components/projectCard/projectCard.js
@@ -14,8 +14,8 @@ const createProjectCards = () => {
           <h5 class="card-title">${project.title}</h5>
           <p class="card-text">${project.description}</p>
           <p class="card-text">Tech Used: ${project.technologiesUsed}</p>
+          <p id="app">Click Link To View App:</p>
           <a class="card-text" href="${project.url}">${project.url}</a>
-          <a class="card-text" href="${project.githubUrl}">${project.githubUrl}</a>
         </div>
       </div>`;
       });
